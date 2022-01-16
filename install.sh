1a4e877a8264ad99fa7469fc9759a870f3702876
CMHash <64366826+CMHash@users.noreply.github.com>
2022-01-16 16:23:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
 
-echo "deb https://apt.sonarr.tv/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+echo "deb http://apt.sonarr.tv/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 
 sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
 
